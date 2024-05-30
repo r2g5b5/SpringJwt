ccd3a74c615996737def4d511f299f89eef973a6
Hazhir Rahmani <programmer4906@gmail.com>
2024-05-30 05:01:19 +0330

    fix bugs

--- src/main/java/com/example/springjwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springjwt/config/JwtAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
         final String username;
-        if (authHeader == null || authHeader.startsWith("Bearer ")) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
