3fe3e33b6100cf6b21dcba8c5535d789ef060fdd
Hazhir Rahmani <programmer4906@gmail.com>
2024-05-30 05:14:24 +0330

    fix bugs

--- src/main/java/com/example/springjwt/config/JwtService.java
+++ src/main/java/com/example/springjwt/config/JwtService.java
@@ -36,7 +36,7 @@ public class JwtService {
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .signWith(getSigningKey())
                 .compact();
     }
 
